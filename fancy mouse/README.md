526585492a5b9eff6ae3877083d5a47c335b5bf0
mike239x <mike239x@gmail.com>
2018-02-27 12:13:30 +0100

    1st working prototype, still needs improvement on the performance side

--- fancy mouse/README.md
+++ fancy mouse/README.md
@@ -1,1 +1,8 @@
-some text here
+The idea is to make a fancy trail following your cursor.
+
+To produce what I actually want to make do the following:
+take a game of life with special rules, namely B3/S12345 (Maze)
+(a cell with 3 neighbors comes to life, a cell with 1 <= n < 6 neighbors stays)
+If you now imagine that your mouse has a circle around it that erases the GOL
+cells and observe how maze closes behind the cursor as it moves you will get
+a lovely pattern.
--- fancy mouse/app.py
+++ fancy mouse/app.py
@@ -2,7 +2,7 @@ import sys, pygame
 from math import *
 
 # expected size of the window
-size = width, height = 600, 400
+size = width, height = 600, 400 #600, 400
 
 class color:
     red = 250, 50, 50
@@ -13,28 +13,64 @@ class color:
     lgray = 210,210,210
     white = 250,250,250
 
-maze_layer = [[0]*height for _ in range(width)]
+maze_layer = {}
 
-def draw_maze():
+def draw_maze(s):
+    # pixObj = pygame.PixelArray(s)
     screen.fill(color.white)
-    screen.fill(color.white)
-    for i in range(width):
-        for j in range(height):
-            if maze_layer[i][j]:
-                # set color of the pixel (i,j) to black
-                pass
+    for pos in maze_layer:
+        pygame.draw.line(s, color.black, pos, pos)
+        #i,j = pos
+        # pixObj[i][j] = color.black
+        # set color of the pixel (i,j) to black
+    # del pixObj
 
 pygame.init()
 screen = pygame.display.set_mode(size)
+cursor_size = 15 # pix
+
+def on_screen(x,y):
+    return x in range(width) and y in range(height)
+
+def maze_iter():
+    delete = {}
+    for pos in maze_layer:
+        i,j = pos
+        n = 0 # neightbors
+        for di in range(-1,2):
+            for dj in range(-1,2):
+                if (i+di,j+dj) in maze_layer:
+                    n += 1
+        if n < 6:
+            delete[pos] = True
+    for pos in delete:
+        del maze_layer[pos]
+
+def draw_circle(x,y):
+    c = cursor_size
+    for dx in range(-c, c+1):
+        for dy in range(-c,c+1):
+            if dx*dx+dy*dy > c*c: continue
+            px = x+dx
+            py = y+dy
+            if not on_screen(px,py): continue
+            maze_layer[px,py] = 1
 
 while 1:
-
-    draw_maze()
-    pygame.display.flip()
-
+    draw_maze(screen)
+    maze_iter()
+    pygame.display.update()
+    # print('step')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if even.type == pygame.MOUSEMOTION:
-            pass
+        if event.type == pygame.KEYUP:
+            if pygame.key.name(event.key) == 'c':
+                maze_layer.clear()
+            else:
+                print(maze_layer)
+        if event.type == pygame.MOUSEMOTION:
+            x,y = event.pos
+            draw_circle(x,y)
+            # pygame.display.flip()
